f92aee9d13b9d332d8704bb49e67bba762f43496
dnedrow <dnedrow@me.com>
2018-09-30 19:58:41 -0400

    Added initial iOS project files.

---
+++ Implementations/ios/ForTheBirds/ForTheBirds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForTheBirds
+//
+//  Created by David Nedrow on 2018-09-30.
+//  Copyright © 2018 Zocalo Devs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Implementations/ios/MVVM-C_Kit/MVVM-C_Kit/MVVM_C_Kit.h
@@ -0,0 +1,19 @@
+//
+//  MVVM_C_Kit.h
+//  MVVM-C_Kit
+//
+//  Created by David Nedrow on 2018-09-30.
+//  Copyright © 2018 Zocalo Devs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVVM_C_Kit.
+FOUNDATION_EXPORT double MVVM_C_KitVersionNumber;
+
+//! Project version string for MVVM_C_Kit.
+FOUNDATION_EXPORT const unsigned char MVVM_C_KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVVM_C_Kit/PublicHeader.h>
+
+
